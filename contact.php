7fa7bc354bef3c1b07202fe5a064cd13cb61ed12
NabilahSumip17 <164476989+NabilahSumip17@users.noreply.github.com>
2024-06-06 20:49:13 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="menu.php">Menu</a></li>
-                <li><a href="about.php">About Us</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
                 <li><a href="contact.php">Contact</a></li>
             </ul>
         </nav>
